27e242b91880ab4babfbb2cf6b6ba577d5d9334f
bilalbash <bilal.basharat@gmail.com>
2020-10-06 14:56:08 +0500

    completed till 34:41

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      {/* <h1>whatsapp clone</h1> */}
       <div className="app__body">
-        {/* Sidebar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>
---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>Sidebar</h1>
+      <div className="sidebar__header">
+
+      </div>
+
+      <div className="sidebar__search">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;

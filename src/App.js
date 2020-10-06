094a6f1c5e12513524280311f8efab8e52096028
bilalbash <bilal.basharat@gmail.com>
2020-10-06 14:39:12 +0500

    completed till 29:05 added yarn instead of npm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      
+    // BEM naming convention
+    <div className="app">
+      {/* <h1>whatsapp clone</h1> */}
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
     </div>
   );
 }

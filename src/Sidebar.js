afb57b92205535ed15d9baddb95d6a1a7271b4b8
Arbab Ali <arbabali1441@gmail.com>
2020-10-08 15:07:57 +0500

    completed till 02:24:29

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,7 +12,7 @@ function Sidebar() {
   const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
-    db.collection('rooms').onSnapshot(onSnapshot =>
+    const unsubscribe = db.collection("rooms").onSnapshot(onSnapshot =>
       setRooms(
         onSnapshot.docs.map((doc) => ({
           id: doc.id,
@@ -20,6 +20,10 @@ function Sidebar() {
         }))
       )
     );
+
+    return () => {
+      unsubscribe();
+    }
   }, []);
 
   return (

0c4e887fe072538b4572062f5e8b084284fc203d
Arbab Ali <arbabali1441@gmail.com>
2020-10-10 10:01:07 +0500

    completed till 02:37:42

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [user, setUser] = useState(null);
+
   return (
     // BEM naming convention
     <div className="app">
-
       {!user ? (
-        <h1>LOGIN</h1>
+        <Login />
       ): (
         <div className="app__body">
           <Router>
